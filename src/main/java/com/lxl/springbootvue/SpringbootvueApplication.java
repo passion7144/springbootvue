a8439e33cdd484b5d1557b33e4ba7eb347ae6b1b
lxl <714416426@qq.com>
2020-11-10 12:18:50 +0800

    2020.11.10 1.0 lxl

---
+++ src/main/java/com/lxl/springbootvue/SpringbootvueApplication.java
@@ -0,0 +1,13 @@
+package com.lxl.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootvueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootvueApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lxl/springbootvue/entity/User.java
@@ -0,0 +1,42 @@
+package com.lxl.springbootvue.entity;
+
+
+public class User {
+    private int userId;
+    private String userDate;
+    private String userName;
+    private String userAddress;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserDate() {
+        return userDate;
+    }
+
+    public void setUserDate(String userDate) {
+        this.userDate = userDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+}
